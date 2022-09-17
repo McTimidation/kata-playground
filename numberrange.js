571bf0155858ad436f237adf3ff1fb4700e414ea
McTimidation <90709441+McTimidation@users.noreply.github.com>
2022-09-17 17:57:02 -0400

    Update numberrange.js

--- numberrange.js
+++ numberrange.js
@@ -1,1 +1,7 @@
-
+function generateRange(min, max, step) {
+        let range = [];
+        for (let i = min; i <= max; i+=step) {
+            range.push(i);
+        }
+        return range;
+    }
