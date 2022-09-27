0afcac2516206cb1f19690521654439fc78be384
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-27 12:20:56 +0000

    kata solved

--- main.js
+++ main.js
@@ -43,33 +43,36 @@ function findLongest(str) {
     return longest
 }
 
-function coinCombo(cents) {
+// function coinCombo(cents) {
 
-    let cents = []
+//     let cents = []
 
-    let quart = 0;
-    let dime = 0;
-    let nick = 0;
-    let penny = 0;
+//     let quart = 0;
+//     let dime = 0;
+//     let nick = 0;
+//     let penny = 0;
 
-    if (cents >= 25) {
-        quart = Math.floor(cents / 25);
-        cents = cents - (25 * quart);
-    }
-    if (cents >= 10){
-        dime = Math.floor(cents / 10);
-        cents = cents - (dime * 10);
-    }
-    if (cents >= 5){
-        nick = Math.floor(cents / 5);
-        cents = cents - (nick * 5);
-    }
-    if( cents >= 0){
-        pennies = cents;
-    }
-    return [penny, dime, nick, quart]
+//     if (cents >= 25) {
+//         quart = Math.floor(cents / 25);
+//         cents = cents - (25 * quart);
+//     }
+//     if (cents >= 10) {
+//         dime = Math.floor(cents / 10);
+//         cents = cents - (dime * 10);
+//     }
+//     if (cents >= 5) {
+//         nick = Math.floor(cents / 5);
+//         cents = cents - (nick * 5);
+//     }
+//     if (cents >= 0) {
+//         pennies = cents;
+//     }
+//     return [penny, dime, nick, quart]
+// }
+
+function noSpace(x) {
+
+        return x.replace(/\s+/g,'');
 }
-
-
 
 
