499e50e36dca029530f3699595a6491cb067655a
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-10 13:55:39 +0000

    kata solved

--- main.js
+++ main.js
@@ -248,3 +248,20 @@ function likes(names) {
     console.log(array2);
     return array2.join('');
   }
+
+
+  function findOdd(A) {
+    let arr = [];
+    for (i = 0; i <= A.length; i++) {
+     arr.push(A.filter(x => x==A[i]));
+      console.log(arr[i].length);
+      console.log(arr[i]);
+      console.log(arr[i][0]);
+      console.log(arr[i].length % 2 !== 0);
+      if (arr[i].length % 2 !== 0) {
+        console.log('found an odd');
+        return arr[i][0];
+       }
+      }
+  }
+   
