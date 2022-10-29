e1ebfb6656bb70bdf715d437ff0212391cbb8abc
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-29 18:47:37 +0000

    kata solved

--- main.js
+++ main.js
@@ -272,4 +272,26 @@ function likes(names) {
           return uniqOrder
           }
       
-      
+          function duplicateCount(text){
+            text = text.toLowerCase()
+            console.log(text)
+            let blankArr = [];
+            let finalArr = [];
+            let charArr = text.split('')
+            let removed;
+            for (let i = 0; i < charArr.length - 1; i++) {
+               removed = charArr.splice(i, 1)
+
+                if (charArr.includes(removed[0])) {
+                    blankArr.push(removed[0])
+                 }
+                i -= 1
+              }
+              blankArr.forEach(char => {
+                if (!finalArr.includes(char)) {
+                  finalArr.push(char)
+                }
+              })
+            console.log(blankArr)
+             return finalArr.length
+            }
